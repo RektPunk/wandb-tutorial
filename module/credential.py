a30e3b1c9752d7c3c96d2086939b47c5f27159c6
RektPunk <rektpunk@gmail.com>
2022-10-13 00:28:28 +0900

    feat - change credential

--- module/credential.py
+++ module/credential.py
@@ -7,5 +7,5 @@ class Credential(str, Enum):
     load_dotenv()
     WANDB_API_TOKEN: str = os.environ["WANDB_API_TOKEN"]
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"{self.value}"
